9c57e22bff9edc7fff974cdebcc65312e2804642
Rail Bariev <r.bariev@hightech.group>
2024-04-04 14:28:42 +0300

    [add] Getters and setters

--- test_application/bin/test_application.dart
+++ test_application/bin/test_application.dart
@@ -1,31 +1,134 @@
 void main() {
-  final Animal cat = Cat();
-  print(cat.isAlive);
-  cat.eat();
-  cat.move();
-  // cat.meow();
-  print(cat);
+  final konstantin = Person('Konstantin', 'Kokorin');
+  final ivan = Student('Ivan', 'Petrov');
+  final vasya = StudentSwimmer('Vasya', 'Sidorov');
+  final misha = StudentAthlet('Misha', 'Ivanov');
+
+  final students = [ivan, vasya, misha];
+  for (var student in students) {
+    print(student.whoAmI());
+  }
+  print('');
+  print('Vasya is Object: ${vasya is Object}');
+  print('Vasya is Person: ${vasya is Person}');
+  print('Vasya is Student: ${vasya is Student}');
+  print('Vasya is StudentSwimmer: ${vasya is StudentSwimmer}');
+  print('Vasya is StudentAthlet: ${vasya is StudentAthlet}');
+  print('');
+
+  final someStudent3 = SomeStudent1('Some', 'Student');
+  someStudent3.grades.add(5);
+  someStudent3.name = 'Jon';
+  someStudent3.surname = 'Snow';
+  print(someStudent3.getFullName());
+  print('');
+
+  final someStudent4 = SomeStudent2();
+  // someStudent3.grades.add(5);
+  // someStudent3.name = 'Jon';
+  // someStudent3.surname = 'Snow';
+  print(someStudent4.getFullName());
+  print('');
+
+  ivan.grades.add(4);
+  vasya.grades.add(4);
+
+  vasya.course = 'Flutter';
+  misha.course = 'Dart';
+  print(vasya.study());
+  print('');
+  print(misha.study());
+  print('');
+
+  print(misha.passText);
+  misha.passText = '12345';
+  print(misha.passText);
+  print(misha.hidPass);
+  print('');
+
+  print(konstantin.getFullName());
+  print('');
+  print(ivan.getFullName());
+  print('');
+  print(vasya.getFullName());
+  print('');
+  print(misha.getFullName());
 }
 
-abstract class Animal {
-  bool isAlive;
-  Animal({required this.isAlive});
+class Person {
+  String name;
+  String surname;
 
-  void eat();
-  void move();
-  
-  @override
-  String toString() => 'I am $runtimeType';
+  Person(this.name, this.surname);
+
+  String getFullName() => '$name $surname';
 }
 
-class Cat extends Animal {
-  Cat() : super(isAlive: true);
+class Student extends Person {
+  List<int> grades = [];
+
+  Student(super.name, super.surname);
 
   @override
-  void eat() => print('Chaw chaw');
+  String getFullName() =>
+      '${super.getFullName()} \nStudent has grades: $grades';
+
+  String whoAmI() => 'I am a Student';
+}
+
+mixin Lerner {
+  String? course;
+  String study() => 'Course is $course';
+}
+
+class StudentSwimmer extends Student with Lerner {
+  StudentSwimmer(super.name, super.surname);
 
   @override
-  void move() => print('Jump jump');
+  String getFullName() =>
+      '${super.getFullName()} \n${grades.every((grade) => grade != 2) ? 'You can swim' : 'Can\'t swim'}';
 
-  void meow() => print('Meow meow');
+  @override
+  String whoAmI() => 'I am Student Swimmer';
+}
+
+class StudentAthlet extends Student with Lerner {
+  StudentAthlet(super.name, super.surname);
+  static const minPracticeTime = 1;
+
+  String _passText = 'pass123';
+  String get passText => _passText;
+  String get hidPass => '*' * _passText.length;
+
+  set passText(String text) {
+    if (text.length < 6) {
+      print('Password must have 6 or more characters');
+      return;
+    }
+    _passText = text;
+  }
+
+  @override
+  String whoAmI() => 'I am Student Athlet';
+}
+
+class SomeStudent1 extends StudentSwimmer {
+  SomeStudent1(super.name, super.surname);
+}
+
+class SomeStudent2 with Lerner implements StudentSwimmer {
+  @override
+  List<int> grades = [4];
+
+  @override
+  String name = 'MyName';
+
+  @override
+  String surname = 'MySurname';
+
+  @override
+  String getFullName() => '$surname $name';
+
+  @override
+  String whoAmI() => 'I am SomeStudent2';
 }
