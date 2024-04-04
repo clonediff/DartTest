4d8bd6ff618a1a93b235d853719c218a5cb94ef2
Rail Bariev <r.bariev@hightech.group>
2024-04-04 13:31:40 +0300

    [add] late modifier examples

--- test_application/bin/test_application.dart
+++ test_application/bin/test_application.dart
@@ -1,11 +1,42 @@
-import 'package:test_application/car.dart';
+late int global;
 
 void main() {
-  final myCar = Car(503, 'BMW');
-  print(myCar);
+  global = 1;
+  print(global);
 
-  Car.numOfWheels = 4;
-  myCar.checkMove();
+  print('Simple order pizza');
+  var pizza = Pizza().orderPizza();
+  // pizza.price = 20; // LateInitializationError: Field 'price' has already been initialized.
+  print('Print simple order pizza');
+  print(pizza);
+  print('');
 
-  Car.setWheels(3);
+  print('orderPizza');
+  var pizza1 = Pizza().orderPizza();
+  print('Print orderPizza');
+  print(pizza1);
+  print('');
+
+  print('late orderPizza');
+  late var pizza2 = Pizza().orderPizza();
+  print('print late orderPizza');
+  print(pizza2);
+}
+
+class Pizza {
+  late String name;
+  late final double price;
+
+  Pizza() {
+    name = 'Spring';
+    price = 10.5;
+  }
+
+  String orderPizza() {
+    print('Coocking pizza...');
+    return 'Your pizza $name is ready';
+  }
+
+  @override
+  String toString() => 'Pizza $name and price: $price';
 }
