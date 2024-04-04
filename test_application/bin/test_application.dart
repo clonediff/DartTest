1f771074646138a36f524d58675da24a784d62a1
Rail Bariev <r.bariev@hightech.group>
2024-04-04 10:30:44 +0300

    [add] Exercises #6

--- test_application/bin/test_application.dart
+++ test_application/bin/test_application.dart
@@ -1,32 +1,36 @@
+import 'dart:collection';
+
 void main() {
   // Упражнение 1
-  const list = [56, 59, 53, 75, 62, 61, 75, 65, 59, 62, 64, 53, 54, 62, 69, 53, 55, 62, 54, 66, 55, 57, 58, 75, 72, 55, 51, 56, 71, 66, 57, 56, 59, 73, 68, 57, 50, 54, 62, 68, 59, 64, 59, 59, 71, 68, 57, 54, 53, 72];
-  final mySet = list.toSet();
-  print(mySet);
-  var avg = 0.0;
-  for (var number in mySet) {
-    avg += number;
-  }
-  avg /= mySet.length;
-  print(avg);
+  var pizzaPrices = {
+    'Venice': 289,
+    'Double Cheese': 359,
+    'Roman Margherita': 299,
+  };
 
-  // Упражнение 2
-  const a = {5, 7};
-  const b = {7, 10};
-  final set = a.union(b).difference(a.intersection(b));
-  print(set);
-  var sum = 0;
-  for (var number in set)
-  {
-    sum += number;
+  // Подзадача 1
+  const myOrder = ['Venice', 'Double Cheese', 'Roman Margherita'];
+  var orderPrice = 0;
+  var notFound = <String>[];
+  for (var pizza in myOrder) {
+    var pizzaPrice = pizzaPrices[pizza];
+    if (pizzaPrice == null) {
+      notFound.add(pizza);
+    } else {
+      orderPrice += pizzaPrice;
+    }
   }
-  print(sum);
+  print('Заказано ${myOrder.length - notFound.length} пиццы из ${myOrder.length} на сумму: $orderPrice рублей');
+  for (var notFoundPizza in notFound) {
+    print('$notFoundPizza нет в меню');
+  }
 
-  // Упражнение 3
-  String initialText = "How many sheets could a sheet slitter slit if a sheet slitter could slit sheets?";
-  final List<String> words = [for (var word in initialText.split(RegExp(r'\W'))) if (word.isNotEmpty) word];
-  print(words);
-  final uniqueWords = words.toSet();
-  print(uniqueWords);
-  print(uniqueWords.length);
+  // Подзадача 2
+  pizzaPrices['Pepperoni'] = 199;
+  print('Новое меню: $pizzaPrices');
+
+  // Подзадача 3
+  final sortedPizzaPrices = SplayTreeMap<String, int>((a, b) => a.compareTo(b));
+  sortedPizzaPrices.addEntries(pizzaPrices.entries);
+  print('Сортированное меню по ключам: $sortedPizzaPrices');
 }
