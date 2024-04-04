8bec5e20ad552902526e58303d6d79d84dba27d4
Rail Bariev <r.bariev@hightech.group>
2024-04-04 12:42:16 +0300

    [add] Try Catch, exceptions Examples

--- test_application/bin/test_application.dart
+++ test_application/bin/test_application.dart
@@ -1,55 +1,71 @@
 void main() {
-  exercise1();
-  print('');
-  exercise2();
-  print('');
-  exercise3();
-}
-
-void exercise1(){
-  const ex1Lists = [
-    ['a', 'ab', 'abc'],
-    ['abcde', 'ab', 'abc'],
-    <String>[],
-  ];
-  for (var list in ex1Lists) {
-    print('$list => ${getLengthsSum(list)}');
+  try {
+    print(factorial(-1));
+  } catch (e, s) {
+    if (e is FactorialException) {
+      print(e.errorMessage());
+    }
+    print(s);
+  } finally {
+    // Code
   }
 }
 
-int getLengthsSum(Iterable<String> list) {
-  return list
-    .map((e) => e.length)
-    .fold(0, (sum, element) => sum + element);
-}
-
-void exercise2() {
-  const ex2Lists = [
-    [1, -10, 9, -1],
-    [-1, -2, -3],
-    <int>[],
-    [1, 2],
-  ];
-  for (var list in ex2Lists) {
-    print('$list => ${getPositivesSum(list)}');
+class FactorialException implements Exception {
+  String errorMessage() {
+    return "Input number must be more than zero";
   }
 }
 
-int getPositivesSum(Iterable<int> list) {
-  return list
-    .where((element) => element > 0)
-    .fold(0, (sum, element) => sum + element);
+int factorial (int n) {
+  if (n < 1) throw FactorialException();
+  int fact = 1; 
+  for (var i = 2; i <= n; i++) {
+    fact *= i;
+  }
+  return fact;
 }
 
-void exercise3() {
-  const list = ['dart', 'abc', 'good luck'];
-  print(getStringTransformations(list).toList());
-}
-
-Iterable<int> getStringTransformations(Iterable<String> list) {
-  return list.indexed
-    .map((element) => element.$2.codeUnits
-      .where((ch) => ch != ' '.codeUnits[0])
-      .map((ch) => ch - 'a'.codeUnits[0] + 1)
-      .fold(0, (sum, number) => sum + number) * (element.$1 + 1));
+void tryCatchShowCases() {
+  try {
+    var list = [double.parse('3.14'), 2, 3];
+    print(list);
+  } catch (e) {
+    print('The exception: $e');
+  }
+  
+  try {
+    var list = [double.parse('3,14'), 2, 3];
+    print(list);
+  } catch (e) {
+    print('The exception: $e');
+  }
+  
+  try {
+    var list = [double.parse('3.14'), 2, 3];
+    print('Result: ${list[5]}');
+  } catch (e) {
+    print('The exception: $e');
+  }
+  
+  try {
+    var list = [double.parse('3.14'), 2, 3];
+    print('Result: ${list[5]}');
+  } on RangeError {
+    print('Out of bound of list'); 
+  }catch (e) {
+    print('The exception: $e');
+  }
+  
+  try {
+    var list = [double.parse('3,14'), 2, 3];
+    print('Result: ${list[5]}');
+  } on RangeError {
+    print('Out of bound of list'); 
+  }catch (e, s) {
+    print('The exception: $e');
+    print('Stack Trace: $s');
+  } finally {
+    print('This is Finally and is always executed');
+  }
 }
