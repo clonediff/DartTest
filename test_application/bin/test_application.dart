01172a1cc0a1f362a0e605e6b157b77ee1ee32f5
Rail Bariev <r.bariev@hightech.group>
2024-04-04 14:42:56 +0300

    [add] Exercise 11

--- test_application/bin/test_application.dart
+++ test_application/bin/test_application.dart
@@ -1,11 +1,24 @@
-import 'package:test_application/car.dart';
-
 void main() {
-  Car intCar = Car(1, 'BMW');
-  print(intCar.id.runtimeType);
-  intCar.move('100');
+  const cuboid = Cuboid(1, 2, 3);
+  print('Cuboid Volume = ${cuboid.Volume}');
+  print('Cuboid Surface Area = ${cuboid.SurfaceArea}');
 
-  Car stringCar = Car('1', 'Ford');
-  print(stringCar.id.runtimeType);
-  stringCar.move(100);
+  const cube = Cube(2);
+  print('Cube Volume = ${cube.Volume}');
+  print('Cube Surface Area = ${cube.SurfaceArea}');
+}
+
+class Cuboid {
+  final int length;
+  final int width;
+  final int height;
+
+  const Cuboid(this.length, this.width, this.height);
+
+  int get SurfaceArea => 2 * (length * width + width * height + length * height);
+  int get Volume => length * width * height;
+}
+
+class Cube extends Cuboid {
+  const Cube(int length) : super(length, length, length);
 }
