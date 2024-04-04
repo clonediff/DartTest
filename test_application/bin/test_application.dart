9c8dd754af75a41ee9d7d1029de05202f07ee221
Rail Bariev <r.bariev@hightech.group>
2024-04-04 14:11:24 +0300

    [add] classes, extends, implements, with(mixins)

--- test_application/bin/test_application.dart
+++ test_application/bin/test_application.dart
@@ -1,42 +1,114 @@
-late int global;
-
 void main() {
-  global = 1;
-  print(global);
+  final konstantin = Person('Konstantin', 'Kokorin');
+  final ivan = Student('Ivan', 'Petrov');
+  final vasya = StudentSwimmer('Vasya', 'Sidorov');
+  final misha = StudentAthlet('Misha', 'Ivanov');
 
-  print('Simple order pizza');
-  var pizza = Pizza().orderPizza();
-  // pizza.price = 20; // LateInitializationError: Field 'price' has already been initialized.
-  print('Print simple order pizza');
-  print(pizza);
+  final students = [ivan, vasya, misha];
+  for (var student in students) {
+    print(student.whoAmI());
+  }
+  print('');
+  print('Vasya is Object: ${vasya is Object}');
+  print('Vasya is Person: ${vasya is Person}');
+  print('Vasya is Student: ${vasya is Student}');
+  print('Vasya is StudentSwimmer: ${vasya is StudentSwimmer}');
+  print('Vasya is StudentAthlet: ${vasya is StudentAthlet}');
   print('');
 
-  print('orderPizza');
-  var pizza1 = Pizza().orderPizza();
-  print('Print orderPizza');
-  print(pizza1);
+  final someStudent3 = SomeStudent1('Some', 'Student');
+  someStudent3.grades.add(5);
+  someStudent3.name = 'Jon';
+  someStudent3.surname = 'Snow';
+  print(someStudent3.getFullName());
   print('');
 
-  print('late orderPizza');
-  late var pizza2 = Pizza().orderPizza();
-  print('print late orderPizza');
-  print(pizza2);
+  final someStudent4 = SomeStudent2();
+  // someStudent3.grades.add(5);
+  // someStudent3.name = 'Jon';
+  // someStudent3.surname = 'Snow';
+  print(someStudent4.getFullName());
+  print('');
+
+  ivan.grades.add(4);
+  vasya.grades.add(4);
+
+  vasya.course = 'Flutter';
+  misha.course = 'Dart';
+  print(vasya.study());
+  print('');
+  print(misha.study());
+  print('');
+
+  print(konstantin.getFullName());
+  print('');
+  print(ivan.getFullName());
+  print('');
+  print(vasya.getFullName());
+  print('');
+  print(misha.getFullName());
 }
 
-class Pizza {
-  late String name;
-  late final double price;
+class Person {
+  String name;
+  String surname;
 
-  Pizza() {
-    name = 'Spring';
-    price = 10.5;
-  }
+  Person(this.name, this.surname);
 
-  String orderPizza() {
-    print('Coocking pizza...');
-    return 'Your pizza $name is ready';
-  }
+  String getFullName() => '$name $surname';
+}
+
+class Student extends Person{
+  List<int> grades = [];
+
+  Student(super.name, super.surname);
 
   @override
-  String toString() => 'Pizza $name and price: $price';
+  String getFullName()  => '${super.getFullName()} \nStudent has grades: $grades';
+
+  String whoAmI() => 'I am a Student';
+}
+
+mixin Lerner {
+  String? course;
+  String study() => 'Course is $course';
+}
+
+class StudentSwimmer extends Student with Lerner {
+  StudentSwimmer(super.name, super.surname);
+
+  @override
+  String getFullName() => '${super.getFullName()} \n${grades.every((grade) => grade != 2) ? 'You can swim' : 'Can\'t swim'}';
+
+  @override
+  String whoAmI() => 'I am Student Swimmer';
+}
+
+class StudentAthlet extends Student with Lerner {
+  StudentAthlet(super.name, super.surname);
+  static const minPracticeTime = 1;
+
+  @override
+  String whoAmI() => 'I am Student Athlet';
+}
+
+class SomeStudent1 extends StudentSwimmer {
+  SomeStudent1(super.name, super.surname);
+}
+
+class SomeStudent2 with Lerner implements StudentSwimmer {
+  @override
+  List<int> grades = [4];
+
+  @override
+  String name = 'MyName';
+
+  @override
+  String surname = 'MySurname';
+
+  @override
+  String getFullName() => '$surname $name';
+  
+  @override
+  String whoAmI() => 'I am SomeStudent2';
 }
