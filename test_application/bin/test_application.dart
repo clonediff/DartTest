c015043920b633fef5bffdbfda4024050556c6f4
Rail Bariev <r.bariev@hightech.group>
2024-04-04 11:46:58 +0300

    [add] Using Functions Examples

--- test_application/bin/test_application.dart
+++ test_application/bin/test_application.dart
@@ -1,21 +1,153 @@
-import 'package:test_application/squares.dart';
+String topLevelVariable = 'Top Level Vairable';
 
 void main() {
-  Iterable<int> hundredSquare() sync* {
-    for (int i = 1; true; i++) {
-      yield i * i;
+  Function person = getName;
+  person();
+  person = getLastName;
+  person();
+
+  getPerson(getName);
+  getPerson(getLastName);
+
+  Function myPerson = getNameOrLastName('Konstantin');
+  myPerson();
+
+  Function addTwoNumbers = (int a, int b) {
+    var sum = a + b;
+    print(sum);
+  };
+  addTwoNumbers(2, 3);
+
+  addTwoNumbers = (int a, int b) => print(a + b);
+  addTwoNumbers(2, 3);
+
+  int result = operationNumbers(5, 2, (a, b) => a - b);
+  print(result);
+
+  const listNumbers = [1, 2, 3, 4, 5, 6];
+
+  final doubleElement = (num number) => print(2 * number);
+  listNumbers.forEach(doubleElement);
+
+  final carColor = {
+    'bmw': 'red',
+    'volvo': 'green'
+  };
+
+  carColor.forEach((car, color) => print('$car is $color'));
+
+  final looped = <int>[];
+  for(final x in listNumbers) {
+    looped.add(x * x);
+  }
+  print(looped);
+
+  final mapped = listNumbers.map((x) => x * x);
+  print(mapped);
+
+  final odds = listNumbers.where((element) => element.isOdd);
+  print(odds);
+
+  // Первый элемент сразу кладет в аккумулятор => падает ошибка, если нет элементов
+  final total = listNumbers.reduce((sum, element) {
+      print('$sum + $element');
+      return sum + element;
+    });
+  print(total);
+
+  // Можно задать начальное значение аккумулятора
+  final totalWithFold = listNumbers.fold(0, (sum, element) {
+      print('$sum + $element');
+      return sum + element;
+    });
+  print(totalWithFold);
+
+  const cars = ['bmw', 'volvo', 'mersedes', 'lada'];
+  final bigCars = cars
+    .where((car) => car.length > 4)
+    .map((car) => car.toUpperCase())
+    .toList();
+  print(bigCars);
+
+  final bigCars1 = <String>[];
+  for (var car in cars) {
+    if (car.length > 4) {
+      var upperCase = car.toUpperCase();
+      bigCars1.add(upperCase);
     }
   }
+  print(bigCars1);
 
-  final squares = hundredSquare();
-  for (var square in squares) {
-    print(square);
-    if (square == 50 * 50) break;
+  String variableInsideMain = 'Variable Inside Main';
+
+  void myFunction() {
+    String variableInsideMyFunction = 'Variable Inside My Function';
+    print('Inside My Function');
+
+    void nestedFunction() {
+      String variableInsideNestedFunction = 'Variable Inside Nested Function';
+      print(topLevelVariable);
+      print(variableInsideMain);
+      print(variableInsideMyFunction);
+      print(variableInsideNestedFunction);
+      print('Inside Nested Function');
+    }
+
+    nestedFunction();
   }
 
-  final squares2 = HundredSquares();
-  for (var square in squares2) {
-    print(square);
-    if (square == 50 * 50) break;
+  myFunction();
+
+  Function add = addNumbersWithFunc(4);
+  int addResult = add(6);
+  print(addResult);
+  print(addNumbersWithFunc(5)(3));
+  print(addNumbersWithFunc2(5)(3));
+}
+
+Function addNumbersWithFunc2(int x) {
+  return (int y) => x + y;
+}
+
+Function addNumbersWithFunc(int x) {
+  int sum(int y) {
+    return x + y;
   }
+  return sum;
+}
+
+int operationNumbers(int a, int b, Function operation) => operation(a, b);
+
+void addNumbers(int a, int b) {
+  var sum = a + b;
+  print(sum);
+}
+
+Function getNameOrLastName(String nameOrLastName) {
+  if (nameOrLastName == 'Konstantin') return getName;
+  return getLastName;
+}
+
+void getPerson(Function myFunction) {
+  myFunction();
+}
+
+void getName() {
+  print('Konstantin');
+}
+
+void getLastName() {
+  print('Kokorin');
+}
+
+String circleArea(int radius) {
+  return 'Circle Area is ${3.14 * radius * radius}';
+}
+
+String person({
+  required String firstName,
+  String lasName = 'adasdasd',
+  int? age,
+}) {
+  return 'First Name: $firstName, Last Name: $lasName, Age: $age';
 }
