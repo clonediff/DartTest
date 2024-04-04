25508564ac15eaea3ff6695fee929d7ec9eef25d
Rail Bariev <r.bariev@hightech.group>
2024-04-04 12:24:55 +0300

    [add] Exercises 8

--- test_application/bin/test_application.dart
+++ test_application/bin/test_application.dart
@@ -1,153 +1,55 @@
-String topLevelVariable = 'Top Level Vairable';
-
 void main() {
-  Function person = getName;
-  person();
-  person = getLastName;
-  person();
+  exercise1();
+  print('');
+  exercise2();
+  print('');
+  exercise3();
+}
 
-  getPerson(getName);
-  getPerson(getLastName);
-
-  Function myPerson = getNameOrLastName('Konstantin');
-  myPerson();
-
-  Function addTwoNumbers = (int a, int b) {
-    var sum = a + b;
-    print(sum);
-  };
-  addTwoNumbers(2, 3);
-
-  addTwoNumbers = (int a, int b) => print(a + b);
-  addTwoNumbers(2, 3);
-
-  int result = operationNumbers(5, 2, (a, b) => a - b);
-  print(result);
-
-  const listNumbers = [1, 2, 3, 4, 5, 6];
-
-  final doubleElement = (num number) => print(2 * number);
-  listNumbers.forEach(doubleElement);
-
-  final carColor = {
-    'bmw': 'red',
-    'volvo': 'green'
-  };
-
-  carColor.forEach((car, color) => print('$car is $color'));
-
-  final looped = <int>[];
-  for(final x in listNumbers) {
-    looped.add(x * x);
+void exercise1(){
+  const ex1Lists = [
+    ['a', 'ab', 'abc'],
+    ['abcde', 'ab', 'abc'],
+    <String>[],
+  ];
+  for (var list in ex1Lists) {
+    print('$list => ${getLengthsSum(list)}');
   }
-  print(looped);
+}
 
-  final mapped = listNumbers.map((x) => x * x);
-  print(mapped);
+int getLengthsSum(Iterable<String> list) {
+  return list
+    .map((e) => e.length)
+    .fold(0, (sum, element) => sum + element);
+}
 
-  final odds = listNumbers.where((element) => element.isOdd);
-  print(odds);
-
-  // Первый элемент сразу кладет в аккумулятор => падает ошибка, если нет элементов
-  final total = listNumbers.reduce((sum, element) {
-      print('$sum + $element');
-      return sum + element;
-    });
-  print(total);
-
-  // Можно задать начальное значение аккумулятора
-  final totalWithFold = listNumbers.fold(0, (sum, element) {
-      print('$sum + $element');
-      return sum + element;
-    });
-  print(totalWithFold);
-
-  const cars = ['bmw', 'volvo', 'mersedes', 'lada'];
-  final bigCars = cars
-    .where((car) => car.length > 4)
-    .map((car) => car.toUpperCase())
-    .toList();
-  print(bigCars);
-
-  final bigCars1 = <String>[];
-  for (var car in cars) {
-    if (car.length > 4) {
-      var upperCase = car.toUpperCase();
-      bigCars1.add(upperCase);
-    }
+void exercise2() {
+  const ex2Lists = [
+    [1, -10, 9, -1],
+    [-1, -2, -3],
+    <int>[],
+    [1, 2],
+  ];
+  for (var list in ex2Lists) {
+    print('$list => ${getPositivesSum(list)}');
   }
-  print(bigCars1);
-
-  String variableInsideMain = 'Variable Inside Main';
-
-  void myFunction() {
-    String variableInsideMyFunction = 'Variable Inside My Function';
-    print('Inside My Function');
-
-    void nestedFunction() {
-      String variableInsideNestedFunction = 'Variable Inside Nested Function';
-      print(topLevelVariable);
-      print(variableInsideMain);
-      print(variableInsideMyFunction);
-      print(variableInsideNestedFunction);
-      print('Inside Nested Function');
-    }
-
-    nestedFunction();
-  }
-
-  myFunction();
-
-  Function add = addNumbersWithFunc(4);
-  int addResult = add(6);
-  print(addResult);
-  print(addNumbersWithFunc(5)(3));
-  print(addNumbersWithFunc2(5)(3));
 }
 
-Function addNumbersWithFunc2(int x) {
-  return (int y) => x + y;
+int getPositivesSum(Iterable<int> list) {
+  return list
+    .where((element) => element > 0)
+    .fold(0, (sum, element) => sum + element);
 }
 
-Function addNumbersWithFunc(int x) {
-  int sum(int y) {
-    return x + y;
-  }
-  return sum;
+void exercise3() {
+  const list = ['dart', 'abc', 'good luck'];
+  print(getStringTransformations(list).toList());
 }
 
-int operationNumbers(int a, int b, Function operation) => operation(a, b);
-
-void addNumbers(int a, int b) {
-  var sum = a + b;
-  print(sum);
-}
-
-Function getNameOrLastName(String nameOrLastName) {
-  if (nameOrLastName == 'Konstantin') return getName;
-  return getLastName;
-}
-
-void getPerson(Function myFunction) {
-  myFunction();
-}
-
-void getName() {
-  print('Konstantin');
-}
-
-void getLastName() {
-  print('Kokorin');
-}
-
-String circleArea(int radius) {
-  return 'Circle Area is ${3.14 * radius * radius}';
-}
-
-String person({
-  required String firstName,
-  String lasName = 'adasdasd',
-  int? age,
-}) {
-  return 'First Name: $firstName, Last Name: $lasName, Age: $age';
+Iterable<int> getStringTransformations(Iterable<String> list) {
+  return list.indexed
+    .map((element) => element.$2.codeUnits
+      .where((ch) => ch != ' '.codeUnits[0])
+      .map((ch) => ch - 'a'.codeUnits[0] + 1)
+      .fold(0, (sum, number) => sum + number) * (element.$1 + 1));
 }
