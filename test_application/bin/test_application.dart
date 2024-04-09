b2d4ead743121602b07c43c4a220122442be6388
Rail Bariev <r.bariev@hightech.group>
2024-04-09 10:14:42 +0300

    [add] Event Loop order examples

--- test_application/bin/test_application.dart
+++ test_application/bin/test_application.dart
@@ -1,24 +1,8 @@
 void main() {
-  const cuboid = Cuboid(1, 2, 3);
-  print('Cuboid Volume = ${cuboid.volume}');
-  print('Cuboid Surface Area = ${cuboid.surfaceArea}');
-
-  const cube = Cube(2);
-  print('Cube Volume = ${cube.volume}');
-  print('Cube Surface Area = ${cube.surfaceArea}');
-}
-
-class Cuboid {
-  final int length;
-  final int width;
-  final int height;
-
-  const Cuboid(this.length, this.width, this.height);
-
-  int get surfaceArea => 2 * (length * width + width * height + length * height);
-  int get volume => length * width * height;
-}
-
-class Cube extends Cuboid {
-  const Cube(int length) : super(length, length, length);
+  print('Первая задача - синхронная');
+  Future(() => print('Пятая задача - очередь событий'))
+    .then((value) => print('Шестая задач - синхронная'));
+  Future(() => print('Вторая задача - очередь событий'));
+  Future.microtask(() => print('Третья задача - очередь микрозадач'));
+  print('Четвертая задача - синхронная');
 }
