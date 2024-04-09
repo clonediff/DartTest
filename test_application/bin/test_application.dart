3de4f44f8a94882028b61a23af5967d891fefbdd
clonediff <32036592+clonediff@users.noreply.github.com>
2024-04-09 12:07:12 +0300

    [add] Simple stream listening

--- test_application/bin/test_application.dart
+++ test_application/bin/test_application.dart
@@ -1,27 +1,26 @@
-import 'package:test_application/api_service.dart';
-import 'package:test_application/model/post.dart';
+import 'dart:async';
 
-void main() async {
-  print('Получение постов...');
+StreamSubscription<int>? subscription;
 
-  await ApiService().fetchPosts();
+void main() {
+  final stream = Stream.periodic(Duration(seconds: 1), (tick) => tick);
 
-  final newPost = Post(id: '3', title: 'Post 3', body: 'Body of Post 3');
-  print('\nСоздание нового поста...');
-  await ApiService().createPost(newPost);
+  subscription = stream.listen((data) {
+    print('Получено: $data');
+  });
 
-  print('\nПолучение постов после создания нового поста...');
-  await ApiService().fetchPosts();
+  Future.delayed(Duration(seconds: 5), () {
+    print('Приостановка потока');
+    subscription?.pause();
+  });
 
-  final updateId = '3';
-  final updatedPost =
-      Post(id: updateId, title: 'Update Post 3', body: 'Update Body 3');
-  print('\nОбнволение поста с ID $updateId...');
-  await ApiService().updatePost(updateId, updatedPost);
+  Future.delayed(Duration(seconds: 10), () {
+    print('Возобновление потока');
+    subscription?.resume();
+  });
 
-  print('Удаление поста с ID: 3...');
-  await ApiService().deletePost('3');
-
-  print('\nПолучение постов после удаления поста...');
-  await ApiService().fetchPosts();
+  Future.delayed(Duration(seconds: 15), () {
+    print('Отписка от потока');
+    subscription?.cancel();
+  });
 }
