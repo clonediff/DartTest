92f8f3aef9e7f6e8f5691147dfae1ba7d70d305c
Rail Bariev <r.bariev@hightech.group>
2024-04-09 10:35:31 +0300

    [add] async/await examples

--- test_application/bin/test_application.dart
+++ test_application/bin/test_application.dart
@@ -1,23 +1,58 @@
-void main() {
+Future<void> main() async {
   print('Запуск функции main()');
 
-  final myFuture = Future.delayed(Duration(seconds: 3), () {
-    print('Future начал выполняться');
-    return 'Данные успешно загружены';
-    throw 'Сервер недоступен';
-  });
+  try {
+    final myFuture = await loadData();
+    print(myFuture);
+  } catch (error) {
+    print(error);
+  } finally {
+    print('Future завершил свою работу');
+  }
 
-  myFuture
-    .then((value) => value.split(''))
-    .then((value) => value.map((e) => e.toUpperCase()))
-    .then((value) => print('Результат: $value'))
-    .catchError(
-      (error) => print('Ошибка: $error'), 
-      test: (error) => error.runtimeType == String,
-    )
-    .whenComplete(() => print('Future завершил свою работу'));
 
-  print(myFuture);
+  // myFuture
+  //   .then((value) => value.split(''))
+  //   .then((value) => value.map((e) => e.toUpperCase()))
+  //   .then((value) => print('Результат: $value'))
+  //   .catchError(
+  //     (error) => print('Ошибка: $error'), 
+  //     test: (error) => error.runtimeType == String,
+  //   )
+  //   .whenComplete(() => print('Future завершил свою работу'));
+
+
+  // firstName().then((firstName) {
+  //   return lastName().then((lastName) {
+  //     var fullName = '$firstName $lastName';
+  //     return sayHello(fullName);
+  //   });
+  // }).then((value) => print(value));
+
+  String first = await firstName();
+  String last = await lastName();
+  String hello = await sayHello('$first $last');
+  print(hello);
 
   print('Завершение работы функции main()');
+}
+
+Future<String> loadData() {
+  return Future.delayed(Duration(seconds: 3), () {
+    print('Future начал выполняться');
+    throw 'Сервер недоступен';
+    return 'Данные успешно загружены';
+  });
+} 
+
+Future<String> firstName() {
+  return Future.value('Константин');
+}
+
+Future<String> lastName() {
+  return Future.value('Кокорин');
+}
+
+Future<String> sayHello(String name) {
+  return Future.value('Привет, $name!');
 }
