cf0d970b6da171f5202e6c7847bd303c34dab75a
Rail Bariev <r.bariev@hightech.group>
2024-04-09 10:25:06 +0300

    [add] Future callbacks

--- test_application/bin/test_application.dart
+++ test_application/bin/test_application.dart
@@ -1,8 +1,23 @@
 void main() {
-  print('Первая задача - синхронная');
-  Future(() => print('Пятая задача - очередь событий'))
-    .then((value) => print('Шестая задач - синхронная'));
-  Future(() => print('Вторая задача - очередь событий'));
-  Future.microtask(() => print('Третья задача - очередь микрозадач'));
-  print('Четвертая задача - синхронная');
+  print('Запуск функции main()');
+
+  final myFuture = Future.delayed(Duration(seconds: 3), () {
+    print('Future начал выполняться');
+    return 'Данные успешно загружены';
+    throw 'Сервер недоступен';
+  });
+
+  myFuture
+    .then((value) => value.split(''))
+    .then((value) => value.map((e) => e.toUpperCase()))
+    .then((value) => print('Результат: $value'))
+    .catchError(
+      (error) => print('Ошибка: $error'), 
+      test: (error) => error.runtimeType == String,
+    )
+    .whenComplete(() => print('Future завершил свою работу'));
+
+  print(myFuture);
+
+  print('Завершение работы функции main()');
 }
